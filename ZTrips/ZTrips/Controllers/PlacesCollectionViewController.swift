cf412922c0f17b3c1c32a3cdd06b8de397fa4237
Libranner <libranner@gmail.com>
2019-01-16 00:24:19 +0100

    Add tab bar controller and my places vc

---
+++ ZTrips/ZTrips/Controllers/PlacesCollectionViewController.swift
@@ -0,0 +1,83 @@
+//
+//  PlacesCollectionViewController.swift
+//  ZTrips
+//
+//  Created by Libranner Leonel Santos Espinal on 15/01/2019.
+//  Copyright © 2019 Libranner Leonel Santos Espinal. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class PlacesCollectionViewController: UICollectionViewController {
+  
+  var places : [Place] = [Place]()
+  
+  override var preferredStatusBarStyle : UIStatusBarStyle {
+    return UIStatusBarStyle.lightContent
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    if let patternImage = UIImage(named: "Pattern") {
+      view.backgroundColor = UIColor(patternImage: patternImage)
+    }
+    collectionView?.backgroundColor = UIColor.white
+    collectionView?.contentInset = UIEdgeInsets(top: 50, left: 10, bottom: 10, right: 10)
+    // Set the PinterestLayout delegate
+    if let layout = collectionView?.collectionViewLayout as? CustomLayout {
+      layout.delegate = self
+    }
+    
+    DataDownloader().download { [weak self] (allPlaces) in
+      self?.places = allPlaces!
+      DispatchQueue.main.async {
+        self?.collectionView?.reloadData()
+      }
+    }
+    
+  }
+  
+}
+
+extension PlacesCollectionViewController {
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return places.count
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AnnotatedPhotoCell", for: indexPath)
+    if let annotateCell = cell as? PlaceCell {
+      annotateCell.place = places[indexPath.item]
+    }
+    return cell
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    let place = getPlace(index: indexPath.item)
+    print(" Place: \(place!.name)")
+  }
+  
+  func getPlace(index: Int) -> Place? {
+    if(index < places.count) {
+      return places[index]
+    }
+    return nil
+  }
+}
+
+extension PlacesCollectionViewController : CustomLayoutDelegate {
+  func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath:IndexPath) -> CGFloat {
+    
+    if indexPath.item % 2 == 0 {
+      return 300
+    }
+    else if indexPath.item % 3 == 0 {
+      return 400
+    }
+    return 500
+    //return places[indexPath.item].image.size.height
+  }
+}
---
+++ ZTrips/ZTrips/VIews/PlaceCell.swift
@@ -0,0 +1,34 @@
+//
+//  AnnotatedPhotoCell.swift
+//  ZTrips
+//
+//  Created by Libranner Leonel Santos Espinal on 15/01/2019.
+//  Copyright © 2019 Libranner Leonel Santos Espinal. All rights reserved.
+//
+
+import UIKit
+
+class PlaceCell: UICollectionViewCell {
+  
+  @IBOutlet fileprivate weak var containerView: UIView!
+  @IBOutlet fileprivate weak var imageView: AsyncImageView!
+  @IBOutlet fileprivate weak var captionLabel: UILabel!
+  @IBOutlet fileprivate weak var commentLabel: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    containerView.layer.cornerRadius = 6
+    containerView.layer.masksToBounds = true
+  }
+  
+  var place: Place? {
+    didSet {
+      if let place = place {
+        imageView.fillWithURL(place.mainImageUrl, placeholder: nil)
+        captionLabel.text = place.name
+        commentLabel.text = place.summary
+      }
+    }
+  }
+  
+}
