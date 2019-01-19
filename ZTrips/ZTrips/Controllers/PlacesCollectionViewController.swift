50417b959cde9fdb0d4fccc5f00c97a3e9bbae53
Libranner <libranner@gmail.com>
2019-01-19 20:07:25 +0100

    More UI fixes

--- ZTrips/ZTrips/Controllers/PlacesCollectionViewController.swift
+++ ZTrips/ZTrips/Controllers/PlacesCollectionViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
 private let reuseIdentifier = "Cell"
 
 class PlacesCollectionViewController: UICollectionViewController {
   
-  var places : [Place] = [Place]()
+  var places : [Place] = [Place]()  
   
   override var preferredStatusBarStyle : UIStatusBarStyle {
     return UIStatusBarStyle.lightContent
@@ -93,7 +94,6 @@ extension PlacesCollectionViewController : CustomLayoutDelegate {
       return 400
     }
     return 500
-    //return places[indexPath.item].image.size.height
   }
 }
 
@@ -104,3 +104,74 @@ extension PlacesCollectionViewController: FiltersTableViewControllerDelegate {
     //fetchNearbyPlaces(coordinate: mapView.camera.target)
   }
 }
+
+/*
+// MARK: - NSFetchedResultsControllerDelegate
+extension PlacesCollectionViewController : NSFetchedResultsControllerDelegate {
+  
+  lazy var fetchedResultsController: NSFetchedResultsController<Country>! = {
+    
+    let fetchRequest: NSFetchRequest<Country> = Country.fetchRequest()
+    
+    // Set the batch size to a suitable number.
+    fetchRequest.fetchBatchSize = 20
+    
+    // Edit the sort key as appropriate.
+    let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+    
+    fetchRequest.sortDescriptors = [sortDescriptor]
+    
+    // Edit the section name key path and cache name if appropriate.
+    // nil for section name key path means "no sections".
+    let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext!, sectionNameKeyPath: nil, cacheName: "Master")
+    
+    aFetchedResultsController.delegate = self
+    
+    do {
+      try aFetchedResultsController.performFetch()
+    } catch {
+      // Replace this implementation with code to handle the error appropriately.
+      // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+      let nserror = error as NSError
+      fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+    }
+    
+    return aFetchedResultsController
+  }()
+  
+  func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+    tableView.beginUpdates()
+  }
+  
+  func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+    switch type {
+    case .insert:
+      tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
+    case .delete:
+      tableView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
+    default:
+      return
+    }
+  }
+  
+  func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+    switch type {
+    case .insert:
+      tableView.insertRows(at: [newIndexPath!], with: .fade)
+    case .delete:
+      tableView.deleteRows(at: [indexPath!], with: .fade)
+    case .update:
+      configureCell(tableView.cellForRow(at: indexPath!)! as! CountryCell,
+                    withCountry: anObject as! Country)
+    case .move:
+      configureCell(tableView.cellForRow(at: indexPath!)! as! CountryCell,
+                    withCountry: anObject as! Country)
+      tableView.moveRow(at: indexPath!, to: newIndexPath!)
+    }
+  }
+  
+  func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+    tableView.endUpdates()
+  }
+}
+*/
